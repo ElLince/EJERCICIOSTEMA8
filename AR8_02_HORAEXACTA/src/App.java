a1fa9f581b9d0848b6093b4f9798f1bdc28fa0f3
Javier León <104456566+ElLince@users.noreply.github.com>
2023-03-29 20:45:23 +0200

    Add files via upload

---
+++ AR8_02_HORAEXACTA/src/App.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        HoraExacta r = new HoraExacta(11, 15,23);
+        System.out.println(r);
+        for (int i = 0; i <=61; i++) {
+            r.inc();
+        }
+        System.out.println("Escriba los segundos ");
+        int segundos = new Scanner(System.in).nextInt();
+        if (r.setSegundos (segundos)){
+            System.out.println(r);
+        }else{
+            System.out.println("No es posible cambiar los segundos");
+        }
+
+        }
+    }
+
+
+
---
+++ AR8_02_HORAEXACTA/src/HoraExacta.java
@@ -0,0 +1,31 @@
+public class HoraExacta extends Hora {
+    protected int segundos;
+    public HoraExacta(int hora, int minuto, int segundo) {
+        super(hora, minuto);
+        if (!setSegundos(segundo)) {
+            System.out.println("Segundos incorrectos");
+        }
+    }
+    public boolean setSegundos(int segundos) {
+        boolean correcto = false;
+        if (segundos >= 0 && segundos < 60) {
+            this.segundos = segundos;
+            correcto = true;
+        }
+            return correcto;
+    }   
+
+    public void inc() {
+        segundos++;
+        if (segundos > 59) {
+            segundos = 0;
+            super.inc();
+        }
+    }
+    public String toString(){
+        String result = super.toString();
+        result += ":" + segundos;
+        return result;
+    }
+
+}
