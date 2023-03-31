f169970d0dc47669fe0c4f7b8643890bb4713ef9
Javier León <104456566+ElLince@users.noreply.github.com>
2023-03-31 20:35:07 +0200

    Add files via upload

---
+++ AR8_19 extends Lista/src/App.java
@@ -0,0 +1,11 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+
+        Lista l1 = new Lista();
+        Conjunto c = new Conjunto();
+        c.insertarPrincipio(1);
+        c.insertarPrincipio(2);
+        c.insertarPrincipio(3);
+        c.mostrar();
+    }
+}
---
+++ AR8_19 extends Lista/src/Conjunto.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+
+public class Conjunto extends Lista{
+
+    public Conjunto() {
+        super();
+    }
+    
+    public Void insertarPrincipio(Integer nuevo){
+     return super.insertarPrincipio(nuevo);
+
+    }
+
+    
+}
