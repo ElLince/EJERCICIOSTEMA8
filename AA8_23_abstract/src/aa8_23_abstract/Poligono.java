ddaf5299c5445ab59490048a7d3404a0ee0b8342
Javier León <104456566+ElLince@users.noreply.github.com>
2023-04-03 19:52:19 +0200

    Add files via upload

---
+++ AA8_23_abstract/src/aa8_23_abstract/Poligono.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package aa8_23_abstract;
+
+/**
+ *
+ * @author jlu
+ */
+public abstract class Poligono {
+    
+    double base;
+    double altura;
+    
+    
+    abstract void area();
+       
+    
+}
